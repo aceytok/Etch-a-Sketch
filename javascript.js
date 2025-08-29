f7a29e07702e75436e69fe130a247ea8f68012fe
Ace Manuyag <119895692+aceytok@users.noreply.github.com>
2025-08-28 19:47:05 -0600

    Add button for changing the number of squares in the grid

--- javascript.js
+++ javascript.js
@@ -1,17 +1,39 @@
 const container = document.querySelector("#container");
 const gridButton = document.querySelector("#gridSize");
+
 /* 
 Create 16X16 grid in container using flexbox
 */
 
+function createGrid(gridSize) {
+    container.innerHTML = ""
 
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let boxSize = 512 / gridSize;
+        const gridBox = document.createElement("div")
 
-for (let i = 0; i< 256; i++) {
-    const gridBox = document.createElement("div")
-    gridBox.setAttribute("class", "box");
-    container.appendChild(gridBox);
-    gridBox.addEventListener('mouseenter', () => {
+        gridBox.setAttribute("class", "box");
+        gridBox.style.width = `${boxSize}px`
+        gridBox.style.height = `${boxSize}px`
+
+        container.appendChild(gridBox);
+
+        gridBox.addEventListener('mouseenter', () => {
         gridBox.style.backgroundColor = "black"
     });
+    };
 };
+
+createGrid(16);
+
+gridButton.addEventListener("click", () => {
+    let input = prompt("How many squares would you like per side? (Maximum 100)");
+    let parsedInput = parseInt(input);
+    if (parsedInput <=100 && parsedInput > 0 && !isNaN(parsedInput)) {
+        createGrid(parsedInput);
+    }
+    else {
+        alert("Please enter a number from 1 to 100")
+    }
+});
 
